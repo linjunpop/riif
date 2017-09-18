dd305abc9ad1bc4ee24cd4ed9563aa3ea87ba8b6
Jonathan Bartlett <jonathan.l.bartlett@gmail.com>
2017-09-18 11:56:21 -0500

    Fixed for Rails 2.3

--- lib/riif.rb
+++ lib/riif.rb
@@ -1,6 +1,10 @@
 require 'riif/version'
 require 'riif/iif'
-require 'riif/railtie' if defined?(Rails)
+if defined?(Rails)
+	if Rails::VERSION::MAJOR > 2
+		require 'riif/railtie'
+	end
+end
 
 module Riif
 end
